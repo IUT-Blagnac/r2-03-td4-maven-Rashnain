6111d5f8f83fee7a2add6e167ace0d4741e782fe
IUTB <noreply@univ-tlse2.fr>
2022-02-25 11:34:12 +0100

    début commentaires

--- src/main/java/lesschtroumpfs/Schtroumpf.java
+++ src/main/java/lesschtroumpfs/Schtroumpf.java
@@ -6,7 +6,11 @@ public class Schtroumpf {
 	private boolean content;
 	private Village village;
 
-
+	/** Constructeur à deux arguments
+	 * Asigne au Schtroumpf un nom et un âge
+	 * @param nom IN : nom du Schtroumpf
+	 * @param age IN : âge du Schtroumpf
+	**/
 	public Schtroumpf (String nom, int age) {
 		this.nom = nom;
 		this.age = age;
@@ -14,6 +18,12 @@ public class Schtroumpf {
 		this.village = null;
 	}
 
+	/** Constructeur à trois arguments
+	 * Asigne au Schtroumpf un nom, un âge et un village
+	 * @param nom IN : nom du Schtroumpf
+	 * @param age IN : âge du Schtroumpf
+	 * @param village IN : village auquel est rataché le Schtroumpf
+	**/
 	public Schtroumpf (String nom, int age, Village village) {
 		this.nom = nom;
 		this.age = age;
@@ -21,19 +31,30 @@ public class Schtroumpf {
 		this.village = village;
 	}
 
-
+	/** Renvoit le nom du Schtroumpf
+	 * @return nom du Schtroumpf
+	**/
 	public String getNom() {
 		return this.nom;
 	}
-	
+
+	/** Renvoit l'âge du Schtroumpf
+	 * @return âge du Schtroumpf
+	**/
 	public int getAge() {
 		return this.age;
 	}
-	
+
+	/** Renvoit l'état du Schtroumpf
+	 * @return vrai si le Schtroumpf est content
+	**/
 	public boolean estContent() {
 		return this.content;
 	}
 	
+	/** Renvoit la présentation du Schtroumpf
+	 * @return la présentation du Schtroumpf
+	**/
 	public String toString() {
 		if (this.content) {
 			if (this.village == null)
@@ -49,10 +70,15 @@ public class Schtroumpf {
 		}
 	}
 	
+	/** Affiche la présentation du Schtroumpf
+	**/
 	public void sePresenter() {
 		System.out.println(this.toString());
 	}
 	
+	/** Renvoit le chant du Schtroumpf
+	 * @return le chant du Schtroumpf
+	**/
 	public String leChant() {
 		if (this.content)
 			return "la, la, la Schtroumpf la, la";
@@ -60,27 +86,44 @@ public class Schtroumpf {
 			return "gloups";
 	}
 	
+	/** Affiche le chant du Schtroumpf
+	**/
 	public void chanter() {
 		System.out.println(this.leChant());
 	}
 	
+	/** Fait tourner la Terre très rapidement mais pour un seul Schtroumpf
+	 * Augmente l'âge du Schtroumpf de 1
+	**/
 	public void anniversaire() {
 		this.age ++;
 	}
 	
+	/** Fait manger le Schtroumpf
+	 * Le rend content car il a droit a son plat préféré
+	**/
 	public void manger(int pfQte) {
 		if (pfQte > 0)
 			this.content = true;
 	}
 
+	/** Fait travailler le Schtroumpf
+	 * Le rend mécontent car il doit se prostituer et il n'aime pas trop ça
+	**/
 	public void allerTravailler() {
 		this.content = false;
 	}
 	
+	/** Renvoit le village du Schtroumpf
+	 * @return l'adresse mémoire du Schtroumpf
+	**/
 	public Village getVillage() {
 		return this.village;
 	}
 	
+	/** Défini le village du Schtroumpf
+	 * @param pfV IN : l'adresse mémoire du village
+	**/
 	public void setVillage(Village pfV) {
 		this.village = pfV;
 	}
